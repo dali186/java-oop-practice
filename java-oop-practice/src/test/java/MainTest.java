b716b4b71fa61093c20c67454fb6bc5be2e3284d
JOOWON <wer07032@naver.com>
2023-06-15 19:36:42 +0900

    Implement main entry point

---
+++ java-oop-practice/src/test/java/MainTest.java
@@ -0,0 +1,14 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void main() {
+        //given
+        String[] args = {"3", "2", "1"};
+        //when & then
+        Main.main(args);
+    }
+}
